22ba71f3b05be7eb4439a3af0f1496874d3b0936
Сергей Мищенко <hudson88@yandex.ru>
2021-10-25 18:35:54 +0300

    added about page

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
 //= ./../../node_modules/jquery/dist/jquery.min.js
-
+//= ./libs/swiper-bundle.min.js
 
 //= ./partials/main.js
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-
-})
+window.addEventListener("DOMContentLoaded", function () {
+  const aboutSLider = new Swiper(".about__slider", {
+    spaceBetween: 16,
+    slidesPerView: 1,
+    speed: 700,
+    navigation: {
+      prevEl: ".about-button-prev",
+      nextEl: ".about-button-next",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+    },
+  });
+});
