5ebad1f718ef98bafb8bd24966388b56fbcfbed3
burdms <burdms@gmail.com>
2021-10-26 12:38:28 +0200

    create: technologies card animation

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-arrow-callback */
 window.addEventListener("DOMContentLoaded", function () {
   const aboutSLider = new Swiper(".about__slider", {
     spaceBetween: 16,
@@ -12,4 +13,25 @@ window.addEventListener("DOMContentLoaded", function () {
       type: "bullets",
     },
   });
+
+  if (document.querySelector('.technologies-card__bg--4')) {
+    const cardBg = document.querySelector('.technologies-card__bg--4'),
+      card = cardBg.closest('.technologies-card');
+
+    card.addEventListener('mouseenter', () => {
+      cardBg.classList.remove('technologies-card__mouse-out');
+
+      setTimeout(() => {
+        cardBg.classList.add('technologies-card__mouse-in');
+      }, 1);
+    });
+
+    card.addEventListener('mouseleave', () => {
+      cardBg.classList.remove('technologies-card__mouse-in');
+
+      setTimeout(() => {
+        cardBg.classList.add('technologies-card__mouse-out');
+      }, 1);
+    });
+  }
 });
