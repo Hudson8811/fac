cb1c2dc2f685241c6ad7d4b50fb47d844af54c87
Anton Ivanov <ivanovanton.web@gmail.com>
2021-10-26 17:35:16 +0300

    added map

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,15 +1,15 @@
 window.addEventListener("DOMContentLoaded", function () {
-  const aboutSLider = new Swiper(".about__slider", {
-    spaceBetween: 16,
-    slidesPerView: 1,
-    speed: 700,
-    navigation: {
-      prevEl: ".about-button-prev",
-      nextEl: ".about-button-next",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-    },
-  });
+	const aboutSLider = new Swiper(".about__slider", {
+		spaceBetween: 16,
+		slidesPerView: 1,
+		speed: 700,
+		navigation: {
+			prevEl: ".about-button-prev",
+			nextEl: ".about-button-next",
+		},
+		pagination: {
+			el: ".swiper-pagination",
+			type: "bullets",
+		},
+	});
 });
