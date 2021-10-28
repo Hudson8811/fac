0d6cbdb10228f988972cbf8abcbc32823982723a
Troshin Sergey <serge-troshin@yandex.ru>
2021-10-28 19:00:59 +0300

    коммит

--- src/js/partials/hero.js
+++ src/js/partials/hero.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', () => {
+  let isMobile = document.documentElement.clientWidth < 768;
   const hero = document.querySelector('.hero');
 
   if (hero) {
@@ -23,129 +24,154 @@ window.addEventListener('load', () => {
     let isMouseEnter = false;
     let step = 0;
 
-    asideLeft.addEventListener('mouseenter', () => {
-      isMouseEnter = true;
-      hero.classList.add(HeroClass.LEFT);
-      step = 1;
+   
+    //console.log('ontouchstart' in window)
 
-      mainDefault.ontransitionend = () => {
-        mainDefault.ontransitionend = null;
-        step = 2;
+    asideLeft.addEventListener((isMobile ? 'click' : 'mouseenter'), showAsideLeft);
 
-        if (step == 2 && isMouseEnter) {
-          mainPlatform.classList.add('is-shown');
-        }
-      }
-    });
+    asideRight.addEventListener((isMobile ? 'click' : 'mouseenter'), showAsideRight);
 
-    asideLeft.addEventListener('mouseleave', () => {
-      isMouseEnter = false;
+    function showAsideLeft(e) {
+      //console.log(e)
+      //if (e.target.closest('.hero__aside--left')) {
+        asideLeft.removeEventListener((isMobile ? 'click' : 'mouseenter'), showAsideLeft);
+        (isMobile ? document : asideLeft).addEventListener((isMobile ? 'click' : 'mouseleave'), hideAsideLeft);
+        
+        isMouseEnter = true;
+        hero.classList.add(HeroClass.LEFT);
+        step = 1;
 
-      if (step == 2 && !isMouseEnter) {
-        mainPlatform.classList.remove('is-shown');
-
-        mainPlatform.ontransitionend = () => {
-          mainPlatform.ontransitionend = null;
-          step = 1;
-
-          if (step == 1 && !isMouseEnter) {
-            hero.classList.remove(HeroClass.LEFT);
-          }
-        }
-      }
-
-      if (step == 1 && !isMouseEnter) {
-        hero.classList.remove(HeroClass.LEFT);
-      }
-    });
-
-    asideRight.addEventListener('mouseenter', () => {
-      isMouseEnter = true;
-
-      hero.classList.add(HeroClass.RIGHT);
-      step = 1;
-
-      mainDefault.ontransitionend = () => {
-        step = 2;
-
-        if (step === 2 && isMouseEnter) {
+        mainDefault.ontransitionend = () => {
           mainDefault.ontransitionend = null;
+          step = 2;
 
-          mainProjects.classList.add('is-shown');
+          if (step == 2 && isMouseEnter) {
+            mainPlatform.classList.add('is-shown');
+          }
+        }
+      //}
+    }
 
-          mainProjectsImage.ontransitionend = () => {
-            step = 3;
-            if (step === 3 && isMouseEnter) {
-              mainProjectsImage.ontransitionend = null;
+    function hideAsideLeft(e) {
+      if (!isMobile || (isMobile && !e.target.closest('.hero__aside--left') || isMobile && e.target.closest('.hero__aside-close'))) {
+        asideLeft.addEventListener((isMobile ? 'click' : 'mouseenter'), showAsideLeft);
 
-              clearInterval(intervalId);
+        (isMobile ? document : asideLeft).removeEventListener((isMobile ? 'click' : 'mouseleave'), hideAsideLeft);
 
-              intervalId = setInterval(() => {
-                if (active.nextElementSibling) {
-                  active.classList.remove('active');
-                  active.nextElementSibling.classList.add('active');
-                  active = active.nextElementSibling;
-                } else {
-                  clearInterval(intervalId);
-                }
-              }, 1000 / 60);
+        isMouseEnter = false;
+
+        if (step == 2 && !isMouseEnter) {
+          mainPlatform.classList.remove('is-shown');
+
+          mainPlatform.ontransitionend = () => {
+            mainPlatform.ontransitionend = null;
+            step = 1;
+
+            if (step == 1 && !isMouseEnter) {
+              hero.classList.remove(HeroClass.LEFT);
             }
-
           }
         }
 
+        if (step == 1 && !isMouseEnter) {
+          hero.classList.remove(HeroClass.LEFT);
+        }
       }
-      
-    });
+    }
+    function showAsideRight(e) {
+      //if (e.target.closest('.hero__aside--right')) {
+        asideRight.removeEventListener((isMobile ? 'click' : 'mouseenter'), showAsideRight);
+        (isMobile ? document : asideRight).addEventListener((isMobile ? 'click' : 'mouseleave'), hideAsideRight);
 
-    asideRight.addEventListener('mouseleave', e => {
-      isMouseEnter = false;
-      
-      if (step === 3 && !isMouseEnter) {
-        clearInterval(intervalId);
-      
-        intervalId = setInterval(() => {
-          if (active.previousElementSibling) {
-            active.classList.remove('active');
-            active.previousElementSibling.classList.add('active');
-            active = active.previousElementSibling;
-          } else {
-            clearInterval(intervalId);
-            step = 2;
+        isMouseEnter = true;
 
-            if (step === 2 && !isMouseEnter) {
-              mainProjects.classList.remove('is-shown');
-              
-              mainProjects.ontransitionend = () => {
-                mainProjects.ontransitionend = null;
-                step = 1;
+        hero.classList.add(HeroClass.RIGHT);
+        step = 1;
 
-                if (step === 1 && !isMouseEnter) {
-                  hero.classList.remove(HeroClass.RIGHT);
+        mainDefault.ontransitionend = () => {
+          step = 2;
+
+          if (step === 2 && isMouseEnter) {
+            mainDefault.ontransitionend = null;
+
+            mainProjects.classList.add('is-shown');
+
+            mainProjectsImage.ontransitionend = () => {
+              step = 3;
+              if (step === 3 && isMouseEnter) {
+                mainProjectsImage.ontransitionend = null;
+
+                clearInterval(intervalId);
+
+                intervalId = setInterval(() => {
+                  if (active.nextElementSibling) {
+                    active.classList.remove('active');
+                    active.nextElementSibling.classList.add('active');
+                    active = active.nextElementSibling;
+                  } else {
+                    clearInterval(intervalId);
+                  }
+                }, 1000 / 60);
+              }
+
+            }
+          }
+        }
+      //}
+    }
+    function hideAsideRight(e) {
+      if (!isMobile || (isMobile && !e.target.closest('.hero__aside--right') || isMobile && e.target.closest('.hero__aside-close'))) {
+        asideRight.addEventListener((isMobile ? 'click' : 'mouseenter'), showAsideRight);
+        (isMobile ? document : asideRight).removeEventListener((isMobile ? 'click' : 'mouseleave'), hideAsideRight);
+
+        isMouseEnter = false;
+        
+        if (step === 3 && !isMouseEnter) {
+          clearInterval(intervalId);
+        
+          intervalId = setInterval(() => {
+            if (active.previousElementSibling) {
+              active.classList.remove('active');
+              active.previousElementSibling.classList.add('active');
+              active = active.previousElementSibling;
+            } else {
+              clearInterval(intervalId);
+              step = 2;
+
+              if (step === 2 && !isMouseEnter) {
+                mainProjects.classList.remove('is-shown');
+                
+                mainProjects.ontransitionend = () => {
+                  mainProjects.ontransitionend = null;
+                  step = 1;
+
+                  if (step === 1 && !isMouseEnter) {
+                    hero.classList.remove(HeroClass.RIGHT);
+                  }
                 }
               }
             }
-          }
-        }, 1000 / 60);
-      }
+          }, 1000 / 60);
+        }
 
-      if (step === 2 && !isMouseEnter) {
-        mainProjects.classList.remove('is-shown');
-        
-        mainProjects.ontransitionend = () => {
-          mainProjects.ontransitionend = null;
-          step = 1;
+        if (step === 2 && !isMouseEnter) {
+          mainProjects.classList.remove('is-shown');
+          
+          mainProjects.ontransitionend = () => {
+            mainProjects.ontransitionend = null;
+            step = 1;
 
-          if (step === 1 && !isMouseEnter) {
-            hero.classList.remove(HeroClass.RIGHT);
+            if (step === 1 && !isMouseEnter) {
+              hero.classList.remove(HeroClass.RIGHT);
+            }
           }
         }
-      }
 
-      if (step === 1 && !isMouseEnter) {
-        hero.classList.remove(HeroClass.RIGHT);
+        if (step === 1 && !isMouseEnter) {
+          hero.classList.remove(HeroClass.RIGHT);
+        }
       }
-    });
+    }
   }
 
   function debounce(func, wait, immediate) {
